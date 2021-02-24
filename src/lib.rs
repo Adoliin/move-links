049d95fde53bf2c8808ba9ca7ceaa032bdef1c18
Adolin <benmeft0@gamil.com>
2021-02-24 20:42:00 +0000

    add test_find_links

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+pub mod cli_utils;
 pub mod paths;
--- tests/unit_test.rs
+++ tests/unit_test.rs
@@ -1,18 +1,72 @@
 //mod paths;
-use move_links::paths::Paths;
-
+use move_links::{cli_utils, paths::Paths};
 use std::{env, process};
 
 #[test]
+fn test_find_links() {
+    init_test_dir();
+    let cwd = get_cwd();
+    let path_list = vec![
+        //files
+        (
+            format!("{}/test-dir/f1", cwd),
+            vec![
+                format!("{}/test-dir/dir1/f1-l1", cwd),
+                format!("{}/test-dir/dir4/dir5/f1-l2", cwd),
+            ],
+        ),
+        (
+            format!("{}/test-dir/dir2/f2", cwd),
+            vec![
+                format!("{}/test-dir/f2-l3", cwd),
+                format!("{}/test-dir/dir2/f2-l2", cwd),
+                format!("{}/test-dir/dir4/f2-l1", cwd),
+            ],
+        ),
+        (
+            format!("{}/test-dir/dir2/f2", cwd),
+            vec![
+                format!("{}/test-dir/dir4/f2-l1", cwd),
+                format!("{}/test-dir/dir2/f2-l2", cwd),
+                format!("{}/test-dir/f2-l3", cwd),
+            ],
+        ),
+        (
+            format!("{}/test-dir/dir4/dir5", cwd),
+            vec![
+                format!("{}/test-dir/dir5-l1", cwd),
+                format!("{}/test-dir/dir4/dir5-l2", cwd),
+                format!("{}/test-dir/dir5-l3", cwd),
+            ],
+        ),
+        (
+            format!("{}/test-dir/dir3", cwd),
+            vec![
+                format!("{}/test-dir/dir1/dir3", cwd),
+                format!("{}/test-dir/dir2/dir3", cwd),
+                format!("{}/test-dir/dir3/dir3", cwd),
+            ],
+        ),
+    ];
+    for path in path_list.iter() {
+        let src_path = &path.0;
+        let expect_link_paths = &path.1;
+        let result_link_paths =
+            cli_utils::find_links(src_path, &format!("{}/test-dir", cwd)).unwrap();
+        let ok = same_vec_items(expect_link_paths, &result_link_paths);
+        //assert_eq!(*expect_link_paths, result_link_paths);
+        let err_msg = format!(
+            "Expected: {:#?}\nResult: {:#?}",
+            expect_link_paths, result_link_paths
+        );
+        assert!(ok, err_msg);
+    }
+}
+
+#[test]
 fn paths_new() {
-    process::Command::new("./scripts/init-test-dir.sh")
-        .output()
-        .unwrap();
-    let cwd = env::current_dir()
-        .unwrap()
-        .into_os_string()
-        .into_string()
-        .unwrap();
+    init_test_dir();
+    let cwd = get_cwd();
     //let test_dir: PathBuf = []
     //env::set_current_dir(PathBuf::from())
 
@@ -66,4 +120,27 @@ fn paths_new() {
         let path_result = Paths::new(&links_dir_param, &src_path_param, &dest_path_param).unwrap();
         assert_eq!(path_expect, path_result);
     }
+}
+
+fn same_vec_items<T: PartialEq>(v1: &Vec<T>, v2: &Vec<T>) -> bool {
+    //check if the Vectors item are the same regardless of order
+    let mut ok = true;
+    for item in v1 {
+        ok = v2.iter().any(|x| x == item);
+    }
+    ok
+}
+
+fn init_test_dir() {
+    process::Command::new("./scripts/init-test-dir.sh")
+        .output()
+        .unwrap();
+}
+
+fn get_cwd() -> String {
+    env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap()
 }
