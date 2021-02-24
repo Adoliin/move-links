cc7072a411d21f2af6be92cd00922fac25fdefd5
Adolin <benmeft0@gamil.com>
2021-02-24 20:42:43 +0000

    edit the test-dir generetor script

--- scripts/init-test-dir.sh
+++ scripts/init-test-dir.sh
@@ -1,44 +1,31 @@
-#!/usr/bin/bash
+#!/usr/bin/env sh
 
 cwd="$(pwd)"
 
 rm -rf test-dir
-mkdir test-dir
-mkdir test-dir/dir1 \
-      test-dir/dir2 \
-      test-dir/dir3 \
-      test-dir/dir4 \
-      test-dir/dir4/dir5 
+mkdir "$cwd/test-dir"      \
+      "$cwd/test-dir/dir1" \
+      "$cwd/test-dir/dir2" \
+      "$cwd/test-dir/dir3" \
+      "$cwd/test-dir/dir4" \
+      "$cwd/test-dir/dir4/dir5"
 
-touch test-dir/f1      \
-      test-dir/dir2/f2 \
-      test-dir/dir3/f3
-      test-dir/dir4/f4
+touch "$cwd/test-dir/f1"      \
+      "$cwd/test-dir/dir2/f2" \
+      "$cwd/test-dir/dir3/f3" \
+      "$cwd/test-dir/dir4/f4"
 
-ln -s "$cwd/test-dir/f1" "$cwd/test-dir/dir1/f1-l1"
-ln -s "$cwd/test-dir/f1" "$cwd/test-dir/dir4/dir5/f1-l2"
-ln -s "$cwd/test-dir/dir2/f2" "$cwd/test-dir/dir4/f2-l1"
-ln -s "$cwd/test-dir/dir2/f2" "$cwd/test-dir/f2-l1"
-ln -s "$cwd/test-dir/dir4/dir5" "$cwd/test-dir"
-ln -s "$cwd/test-dir/dir3" "$cwd/test-dir/dir2"
+ln -s "$cwd/test-dir/f1"        "$cwd/test-dir/dir1/f1-l1"
+ln -s "$cwd/test-dir/f1"        "$cwd/test-dir/dir4/dir5/f1-l2"
 
-#Tree respresentation:
-#
-#test-dir
-#│
-#├── dir1
-#│   └── f1-l1 -> $cwd/test-dir/f1
-#├── dir1
-#│   └── f1-l1 -> $cwd/test-dir/f1
-#├── dir2
-#│   ├── dir3 -> $cwd/test-dir/dir3
-#│   └── f2
-#├── dir3
-#│   └── f3
-#├── dir4
-#│   ├── dir5
-#│   │   └── f1-l2 -> $cwd/move-links/test-dir/f1
-#│   └── f2-l1 -> $cwd/move-links/test-dir/dir2/f2
-#├── dir5 -> $cwd/move-links/test-dir/dir4/dir5
-#├── f1
-#└── f2 -> $cwd/move-links/test-dir/dir2/f2
+ln -s "$cwd/test-dir/dir2/f2"   "$cwd/test-dir/dir4/f2-l1"
+ln -s "$cwd/test-dir/dir2/f2"   "$cwd/test-dir/dir2/f2-l2"
+ln -s "$cwd/test-dir/dir2/f2"   "$cwd/test-dir/f2-l3"
+
+ln -s "$cwd/test-dir/dir4/dir5" "$cwd/test-dir/dir5-l1"
+ln -s "$cwd/test-dir/dir4/dir5" "$cwd/test-dir/dir4/dir5-l2"
+ln -s "$cwd/test-dir/dir4/dir5" "$cwd/test-dir/dir5-l3"
+
+ln -s "$cwd/test-dir/dir3"      "$cwd/test-dir/dir1/dir3"
+ln -s "$cwd/test-dir/dir3"      "$cwd/test-dir/dir2/dir3"
+ln -s "$cwd/test-dir/dir3"      "$cwd/test-dir/dir3/dir3"
