15249b698823c6ce9c2de1c90dcaa5f9a8d40ea4
Adolin <benmeft0@gamil.com>
2021-02-26 19:16:53 +0000

    change cli argument names

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,11 @@ use std::process;
 
 fn main() {
     let matches = get_matches();
-    let links_dir_raw = matches.value_of("links_dir").unwrap();
-    let src_path_raw = matches.value_of("src_path").unwrap();
-    let dest_path_raw = matches.value_of("dest_path").unwrap();
+    let links_dir_raw = matches.value_of("LINKS_DIRECTORY").unwrap();
+    let src_path_raw = matches.value_of("SOURCE").unwrap();
+    let dest_path_raw = matches.value_of("DEST").unwrap();
 
-    let paths = Paths::new(
-        links_dir_raw, src_path_raw, dest_path_raw
-    ).unwrap_or_else(|err| {
+    let paths = Paths::new(links_dir_raw, src_path_raw, dest_path_raw).unwrap_or_else(|err| {
         println!("Error in arguments: {}", err);
         process::exit(1);
     });
@@ -31,24 +29,26 @@ fn main() {
 
 fn get_matches() -> clap::ArgMatches<'static> {
     App::new("Move Links")
-        .version("0.1")
+        .version("0.1.0")
+        .visible_alias("do-stuff")
         .author("Ben Mefteh F. <benmeft0@gmail.com>")
-        .about("Does awesome things")
+        .about("Move (rename) SOURCE to DEST, and redirect all ot its symbolic \
+                links inside of LINKS_DIRECTORY")
         .arg(
-            Arg::with_name("src_path")
-                .help("Sets the input file to use")
+            Arg::with_name("SOURCE")
+                .help("Source file or directory")
                 .required(true)
                 .index(1),
         )
         .arg(
-            Arg::with_name("dest_path")
-                .help("Sets the input file to use")
+            Arg::with_name("DEST")
+                .help("Destination (output) file or directory")
                 .required(true)
                 .index(2),
         )
         .arg(
-            Arg::with_name("links_dir")
-                .help("Sets a custom config file")
+            Arg::with_name("LINKS_DIRECTORY")
+                .help("Directory in which to search for symbolic links of SOURCE")
                 .required(true)
                 .short("-d")
                 .long("links-dir")
